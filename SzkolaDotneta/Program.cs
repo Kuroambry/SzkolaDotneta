ece6ab6728279cace2cf9ea6c6d8dff70732e714
Kuroambry <84273077+Kuroambry@users.noreply.github.com>
2022-02-18 19:29:46 +0100

    Add project files.

---
+++ SzkolaDotneta/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SzkolaDotneta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
